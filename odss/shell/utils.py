a03f394625a7254611a907639d45dd0c0218c357
Piotr Smialkowski <p.smialkowski@gmail.com>
2021-03-06 09:55:31 +0100

    Update ascii table view

--- odss/shell/utils.py
+++ odss/shell/utils.py
@@ -10,7 +10,18 @@ BUNDLE_STATE = {
 def bundle_state_name(state):
     return BUNDLE_STATE[str(state)]
 
+def _sep_top(sizes):
+    line = "┬".join(["─"*(size+2) for size in sizes])
+    return f"┌{line}┐"
+def _sep_mid(sizes):
+    line = "┼".join(["─"*(size+2) for size in sizes])
+    return f"├{line}┤"
+def _sep_bottom(sizes):
+    line = "┴".join(["─"*(size+2) for size in sizes])
+    return f"└{line}┘"
+
 def make_ascii_table(headers, records):
+
     sizes = [len(head) for head in headers]
 
     for row, record in enumerate(records):
@@ -22,22 +33,19 @@ def make_ascii_table(headers, records):
             if sizes[column] < size:
                 sizes[column] = size
 
-    sformat = "|"
+    sformat = "│"
     for size in sizes:
-        sformat += f" {{:^{size}}} |"
+        sformat += f" {{:<{size}}} │"
 
     buff = []
     sheader = sformat.format(*headers)
-    buff.append("-" * len(sheader))
-    buff.append(sheader)
-    buff.append("-" * len(sheader))
+    buff.append(_sep_top(sizes))
 
-    sformat = "|"
-    for size in sizes:
-        sformat += f" {{:<{size}}} |"
+    buff.append(sheader)
+    buff.append(_sep_mid(sizes))
 
     for record in records:
         buff.append(sformat.format(*record))
-    buff.append("-" * len(sheader))
+    buff.append(_sep_bottom(sizes))
 
     return "\n".join(buff)
