9cbee273c5007c43de4afadc012e8004185076ac
Piotr Smialkowski <p.smialkowski@gmail.com>
2022-10-28 23:46:12 +0200

    Remove rubbish

--- odss/_http/__init__.py
+++
@@ -1,9 +0,0 @@
-from .decorators import route, get, post, delete, put, patch, option, head  # noqa
-from odss.http.abc import (
-    IHttpServer,
-    IHttpRequest,
-    IHttpContext,
-    IHttpService,
-    IHttpRouteService,
-    IHttpMiddlewareService,
-)
--- odss/_http/consts.py
+++
@@ -1,1 +0,0 @@
-ODSS_HTTP_ROUTE_HANDLER = "odss.http.route.handler"
