bd030d5e830ae2cc800415b7a727ea8ec4a80a82
Piotr Smialkowski <p.smialkowski@gmail.com>
2022-11-08 21:23:38 +0100

    Init odss.services.terminal

---
+++ odss.services.terminal/odss/services/terminal/bundle.py
@@ -0,0 +1,57 @@
+import asyncio
+
+from odss.common import SERVICE_SHELL, ServiceTracker
+
+from .session import Session
+
+
+class Activator:
+    def __init__(self):
+        pass
+
+    async def start(self, ctx):
+        self.console = InteractiveConsole(ctx)
+        await self.console.start()
+
+    async def stop(self, ctx):
+        await self.console.stop()
+
+
+class InteractiveConsole(ServiceTracker):
+    def __init__(self, ctx):
+        super().__init__(self, ctx, SERVICE_SHELL)
+        self.session = Session(self)
+        self.shell = None
+
+    async def start(self):
+        await self.open()
+
+    async def stop(self):
+        await self.close()
+
+    def get_completions(self, line):
+        if self.shell:
+            return self.shell.get_all_commands()
+        return []
+
+    def on_adding_service(self, reference, service):
+        if not self.shell:
+            self.shell = service
+            self.task = asyncio.create_task(self.run())
+
+    def on_modified_service(self, reference, service):
+        pass
+
+    def on_removed_service(self, reference, service):
+        self.shell = None
+        if not self.task.done():
+            self.task.cancel()
+
+    async def run(self):
+        while self.shell:
+            # try:
+            line = await self.session.readline()
+            if self.shell:
+                await self.shell.execute(line, self.session)
+            # except KeyboardInterrupt:
+            #     break
---
+++ odss.services.terminal/odss/services/terminal/session.py
@@ -0,0 +1,62 @@
+import os
+import typing as t
+
+from prompt_toolkit import PromptSession
+from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
+from prompt_toolkit.completion import Completion
+from prompt_toolkit.history import FileHistory
+from prompt_toolkit.patch_stdout import patch_stdout
+
+
+class ShellCompleter:
+    def __init__(self, completer):
+        self.completer = completer
+
+    def get_completions(self, document, event):
+        word_before_cursor = document.text_before_cursor.lstrip()
+        commands = self.completer.get_completions(word_before_cursor)
+        for name in commands:
+            if name.startswith(word_before_cursor):
+                yield Completion(
+                    name,
+                    -len(word_before_cursor),
+                )
+
+
+class Session:
+    def __init__(self, completer):
+        file_path = os.path.abspath(".odsshistory")
+        self.prompt_session = PromptSession(
+            history=FileHistory(file_path),
+            auto_suggest=AutoSuggestFromHistory(),
+            completer=ShellCompleter(completer),
+            complete_in_thread=True,
+        )
+
+    def write_line(self, data: str, flush=True) -> None:
+        self.write(data)
+        if data[-1] != "\n":
+            self.write("\n")
+        if flush:
+            self.flush()
+
+    def write(self, data) -> None:
+        with patch_stdout():
+            self.prompt_session.output.write(data)
+
+    def flush(self) -> None:
+        with patch_stdout():
+            self.prompt_session.output.flush()
+
+    async def readline(self) -> t.Awaitable[str]:
+        return await self.prompt("$> ")
+
+    async def prompt(self, message) -> t.Awaitable[str]:
+        with patch_stdout():
+            return await self.prompt_session.prompt_async(
+                message,
+                # completer=self.completer,
+                refresh_interval=0.5,
+                # set_exception_handler=False,
+                handle_sigint=False,
+            )
